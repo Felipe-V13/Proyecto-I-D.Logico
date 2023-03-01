b1e31a0a864c6e98c6f16e6987068474edfa8c24
Akai nishin <coreajustin288@gmail.com>
2023-03-01 16:05:17 -0600

    Agregado: Entradas por archivo de texto y generador latex

--- multiDL.py
+++ multiDL.py
@@ -1,29 +1,88 @@
+import sys
 import argparse
 import io
 import subprocess
+class Titlepage:
+    def __init__(self):
+        self.__title = "\\title{}\n"
+        self.__subtitle = "\subtitle{}\n"
+        self.__authors = "\\author{}\n"
+        self.__institute = "\institute{}\n"
+        self.__date = "\date{}\n"
+
+        self.__text = ""
+    
+    def setPageTitleInfo(self, title = "", subtitle = "", authors = "", institute = "", date = ""):
+        self.__title = self.__title.replace("{}", "{"+title+"}")
+        self.__subtitle = self.__subtitle.replace("{}", "{"+subtitle+"}")
+        self.__institute = self.__institute.replace("{}", "{"+institute+"}")
+        self.__authors = self.__authors.replace("{}", "{"+authors+"}")
+        self.__date = self.__date.replace("{}", "{"+date+"}")
+    
+    def getTitlePage(self):
+        self.__text += self.__title
+        self.__text += self.__subtitle
+        self.__text += self.__authors
+        self.__text += self.__institute
+        self.__text += self.__date
+        return self.__text
+
+class Frame:
+    def __init__(self):
+        self.__content = ""
+        self.__frameTitle = ""
+        self.__text = ""
+
+    def setContent(self, title = "", content = ""):
+        self.__frameTitle = title
+        self.__content = content
+    
+    def getFrame(self):
+        self.__text += "\\begin{frame}\n"
+        self.__text += "\\frametitle{"+self.__frameTitle+"}\n"
+        self.__text += "\\begin{center}\n"
+        self.__text += self.__content + "\n"
+        self.__text += "\end{center}\n"
+        self.__text += "\end{frame}\n"
+        return self.__text
+
 class Latex:
     def __init__(self):
         self.__text = ""
         self.__ficheroLatex = io.open("latex.tex", "w")
+        self.__frames = []
+        self.__titlepage = Titlepage()
+        self.__framescounter = 0
+ 
+    def createFrame(self, title, content):
+        self.__frames.append(Frame())
+        self.__frames[self.__framescounter].setContent(title, content)
+        self.__framescounter += 1
+    
+    def setTitlePageContent(self, title = "", subtitle = "", authors = [], institute = "", date = ""):
+        authores = ""
+        for autor in authors:
+            authores += autor + " \\and "
+        self.__titlepage.setPageTitleInfo(title, subtitle, authores, institute, date)
 
-    def Beammer(self, operacion, resultado):
-        self.__text = "\documentclass{beamer}\n"
+    def __maketitle__(self):
+        self.__text += "\\frame{\\titlepage}\n"
+
+    def beamer(self):
+        self.__text += "\documentclass{beamer}\n"
+        self.__text += self.__titlepage.getTitlePage()
         self.__text += "\\begin{document}\n"
-        self.__text += "\\begin{frame}\n"
-        self.__text += "\\frametitle{Operacion}\n"
-        self.__text += "\\begin{center}\n"
-        self.__text +=  operacion +"\n"
-        self.__text += "\end{center}\n"
-        self.__text += "\end{frame}\n"
-        self.__text += "\\begin{frame}\n"
-        self.__text += "\\frametitle{Resultado}\n"
-        self.__text += "\\begin{center}\n"
-        self.__text += resultado + "\n"
-        self.__text += "\end{center}\n"
-        self.__text += "\end{frame}\n"
+        for page in self.__frames:
+            self.__text += page.getFrame()
+        self.__maketitle__()
         self.__text += "\end{document}\n"
-
         self.__ficheroLatex.write(self.__text)
+
+    def CreatePDF(self):
+        subprocess.call()
+
+    def __str__(self):
+        return self.__text
 
     def CreatePDF(self):
         subprocess.run("pdflatex latex.tex")
@@ -50,15 +109,36 @@ def to_base(num, base):
         return int(num)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Multiplicación de números en diferentes bases')
-    parser.add_argument('--bits', type=int, help='Número de bits para la representación de los números', required=True)
-    parser.add_argument('-a', type=str, help='Primer factor a multiplicar', required=True)
-    parser.add_argument('-b', type=str, help='Segundo factor a multiplicar', required=True)
-    args = parser.parse_args()
+    commands_i = sys.argv[1:]
+    parser = argparse.ArgumentParser()
 
-    bits = args.bits
-    a_str = args.a
-    b_str = args.b
+    if len(commands_i) != 0:
+        parser.add_argument('--bits', type = int, help="Numero de bits para representacion numerica", required= False)
+        parser.add_argument('-a', type = str, help = "Multiplicando", required = ('--bits' == commands_i[0]))
+        parser.add_argument('-b', type = str, help = "Operando", required = ('--bits' == commands_i[0]))
+
+        parser.add_argument('-f', type = str, help = "Name file with factors and bits extension for representation", required=False)
+
+    else:
+        print('There aren\'t inputs')
+        sys.exit(-1)
+
+    args = parser.parse_args()
+    bits = ""
+    a_str = ""
+    b_str = ""
+
+    if commands_i[0] == "--bits":
+        bits = args.bits
+        a_str = args.a
+        b_str = args.b
+
+    if commands_i[0] == "-f":
+        with io.open(args.f, 'r') as file:
+            filetext = file.readline().split(' ')
+            bits = int(filetext[1])
+            a_str = filetext[3]
+            b_str = filetext[5]
 
     # Obtener los valores de los factores
     # Identificamos la base del número (b, h, d) y lo convertimos a decimal para operar con ellos
@@ -104,11 +184,4 @@ if __name__ == '__main__':
     else:
         print(f"{' ' * (bits + 4)}{'-' * (bits + 4)}")
         print(f"{' ' * (bits - len(result_str))}{result_str} (resultado)")
-
-
-
-
-
-
-
 
